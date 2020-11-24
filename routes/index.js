251c167c783db8454a9378c6b2c31c7edd407ad0
Carlos Davila <casedaro15@gmail.com>
2020-11-24 11:59:44 +0100

    add filter routes/views/javascripts

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 module.exports = app => {
     const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Log in first' })
-   
-    
+
+
     app.use('/', require('./base.routes.js'))
     app.use('/', require('./auth.routes.js'))
     app.use('/shows', require('./shows.routes.js'))
+    app.use('/api', require('./api.routes.js'))
+    app.use('/filter', require('./filter.routes.js'))
+
+
     app.use('/', ensureAuthenticated, require('./profile.routes.js'))
 
     // API Shows
